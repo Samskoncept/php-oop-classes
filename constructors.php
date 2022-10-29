52ceb4ec4d5c88a162ace51dacbc12cb333284a9
unknown <samuelfrancis90@hotmail.com>
2022-10-29 08:13:14 +0100

    initial commit

---
+++ constructors.php
@@ -0,0 +1,32 @@
+<?php
+
+class Person
+{
+
+    //properties
+    public $name;
+    public $age;
+
+    //method
+    function __construct($name, $age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    function get_name()
+    {
+        return $this->name;
+    }
+
+    function get_age()
+    {
+        return $this->age;
+    }
+}
+
+
+$person = new Person("Samuel", 23);
+echo "Name " . $person->get_name();
+echo "\n";
+echo "Age " . $person->get_age();
